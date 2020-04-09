efa5efbdc983936403842a45b3ef1f5e53b21fb5
Hisanori Kuwauchi <hkuwauchi@gmail.com>
2020-04-09 20:38:45 +0900

    プロジェクト ファイルを追加します。

---
+++ SampleRazorApp/Pages/Find.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SampleRazorApp.Data;
+using SampleRazorApp.Models;
+
+namespace SampleRazorApp.Pages
+{
+    public class FindModel : PageModel
+    {
+        private readonly SampleRazorAppContext _context;
+        public IList<Person> People { get; set; }
+
+        public FindModel(SampleRazorAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            People = await _context.Person.ToListAsync();
+        }
+
+        public async Task OnPostAsync(string Find)
+        {
+            People = await _context.Person.Where(c => c.Name == Find).ToListAsync();
+        }
+    }
+}
